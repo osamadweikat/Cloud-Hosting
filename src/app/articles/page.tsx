83d5ac79bc81471441302138fc0d46cc4341a8fe
osamadweikat <osamadweikat83@gmail.com>
2026-04-11 20:01:38 +0300

    Create articles components

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,7 +1,18 @@
-const ArticlesPage = () => {
-  return (
-    <div>ArticlesPage</div>
-  )
-}
+import ArticleItem from "@/src/components/articles/ArticleItem";
+import { Article } from "../../utils/types";
 
-export default ArticlesPage
+const ArticlesPage = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const articles: Article[] = await response.json();
+  return (
+    <section className="container m-1 px-5">
+      <div className="flex items-center justify-center flex-wrap gap-7">
+        {articles.map((item) => (
+          <ArticleItem article={item} key={item.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ArticlesPage;
