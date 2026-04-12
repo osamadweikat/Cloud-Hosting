74fadec51742828fa5623a6a28dc1f8fa3d3c519
osamadweikat <osamadweikat83@gmail.com>
2026-04-12 11:49:59 +0300

    Create not found page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -8,22 +8,24 @@ interface ErrorPageProps {
 
 const ErrorPage = ({ error, reset }: ErrorPageProps) => {
   return (
-    <div className="pt-7 text-center">
-      <div className="text-3xl text-red-600 font-semibold">
-        Something went wrong
+    <div className="flex justify-center items-center min-h-[calc(100vh-100px)]">
+      <div className="pt-7 text-center">
+        <div className="text-3xl text-red-600 font-semibold">
+          Something went wrong
+        </div>
+        <h2 className="text-gray-700 my-3 text-xl">
+          Error Message: {error.message}
+        </h2>
+        <button
+          onClick={() => reset()}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
+        >
+          Try again
+        </button>
+        <Link className="text-xl underline text-blue-700 block mt-6" href="/">
+          Go to home page
+        </Link>
       </div>
-      <h2 className="text-gray-700 my-3 text-xl">
-        Error Message: {error.message}
-      </h2>
-      <button
-        onClick={() => reset()}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
-      >
-        Try again
-      </button>
-      <Link className="text-xl underline text-blue-700 block mt-6" href="/">
-        Go to home page
-      </Link>
     </div>
   );
 };
---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <section className="flex justify-center items-center flex-col min-h-[calc(100vh-100px)]">
+      <h1 className="text-7xl text-gray-800 font-bold">404</h1>
+      <p className="text-gray-500 text-3xl mt-2 mb-5">Page Not Found</p>
+      <Link className="text-xl underline text-blue-700" href="/">
+        Go to home page
+      </Link>
+    </section>
+  );
+};
+
+export default NotFoundPage;
