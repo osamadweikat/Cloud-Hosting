d84ace9cbd970684f7243ea0eda8167fc131dcbd
osamadweikat <osamadweikat83@gmail.com>
2026-04-13 13:14:32 +0300

    Create web hosting plan section in home page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image"
-import CloudImage from "../../../public/cloud-hosting.png"
-
 const AboutPage = () => {
   return (
     <section className="container m-auto">
-      <h1 className="text-3xl font-bold text-gray-800 p-5">About Page</h1>
-      <div>
-        <Image src={CloudImage} alt="Cloud" width={500} height={500} />
-      </div>
+      <h1 className="text-3xl font-bold text-gray-800 p-5">About This App</h1>
+      <p className="px-5 text-gray-600 text-xl">
+        The best web hosting solution for your online success
+      </p>
+      <div></div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
